0b87dfc6e99eb57489b1d588258567d3ad00ebd3
Celeste Dangelo <celesdv@gmail.com>
2024-05-05 14:47:56 -0300

    genarl fixi

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://celesdv.github.io/latinad-frontend'
+  base: 'http://celesdv.github.io/latinad-frontend'
 })
